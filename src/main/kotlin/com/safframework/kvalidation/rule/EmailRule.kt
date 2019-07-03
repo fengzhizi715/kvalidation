fb69dfd912d67d892ae47169e3acd81cedf0a87c
fengzhizi715 <fengzhizi715@126.com>
2019-07-03 23:03:51 +0800

    add PatternRule

--- src/main/kotlin/com/safframework/kvalidation/rule/EmailRule.kt
+++ src/main/kotlin/com/safframework/kvalidation/rule/EmailRule.kt
@@ -1,19 +1,15 @@
 package com.safframework.kvalidation.rule
 
-import com.safframework.kvalidation.ValidateRule
 import java.util.regex.Pattern
 
 /**
  * Created by tony on 2019-07-03.
  */
-class EmailRule : ValidateRule<String> {
+class EmailRule : PatternRule(emailPattern) {
 
-    override fun errorMessage() = "email must not be null"
-
-    override fun validate(data: String) = emailPattern.matcher(data).matches()
+    override fun errorMessage() = "invalid email address"
 
     companion object {
         val emailPattern = Pattern.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$")
     }
-
 }
---
+++ src/main/kotlin/com/safframework/kvalidation/rule/PatternRule.kt
@@ -0,0 +1,14 @@
+package com.safframework.kvalidation.rule
+
+import com.safframework.kvalidation.ValidateRule
+import java.util.regex.Pattern
+
+/**
+ * Created by tony on 2019-07-03.
+ */
+open class PatternRule(val pattern: Pattern) : ValidateRule<String> {
+
+    override fun errorMessage() = "Pattern $pattern not valid"
+
+    override fun validate(data: String) = pattern.matcher(data).matches()
+}
