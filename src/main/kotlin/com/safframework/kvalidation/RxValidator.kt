85dde88a7c7614ad7fdfc044c1b50f6dcecc1166
fengzhizi715 <fengzhizi715@126.com>
2019-07-03 20:58:42 +0800

    update RxValidator

--- src/main/kotlin/com/safframework/kvalidation/RxValidator.kt
+++ src/main/kotlin/com/safframework/kvalidation/RxValidator.kt
@@ -1,19 +1,40 @@
 package com.safframework.kvalidation
 
+import io.reactivex.Flowable
+import io.reactivex.Maybe
 import io.reactivex.Observable
+import io.reactivex.Single
 
 /**
  * Created by tony on 2019-07-03.
  */
 class RxValidator<T>(private val data: T) : Validator<T>() {
 
-    fun toObservable(success: (() -> Unit)? = null,error: ((String) -> Unit)? = null) = Observable.create<T> {
-        it.onNext(data)
-    }.map {
-        validate(it, onSuccess = {
-            success?.invoke()
-        }, onError = { message ->
-            error?.invoke(message)
-        })
-    }
+    fun toObservable(success: (() -> Unit)? = null,error: ((String) -> Unit)? = null) =
+        Observable.just(data)
+            .map {
+                validate(it, onSuccess = { success?.invoke() }, onError = { message -> error?.invoke(message)
+                })
+            }
+
+    fun toFlowable(success: (() -> Unit)? = null,error: ((String) -> Unit)? = null) =
+        Flowable.just(data)
+            .map {
+                validate(it, onSuccess = { success?.invoke() }, onError = { message -> error?.invoke(message)
+                })
+            }
+
+    fun toSingle(success: (() -> Unit)? = null,error: ((String) -> Unit)? = null) =
+        Single.just(data)
+            .map {
+                validate(it, onSuccess = { success?.invoke() }, onError = { message -> error?.invoke(message)
+                })
+            }
+
+    fun toMaybe(success: (() -> Unit)? = null,error: ((String) -> Unit)? = null) =
+        Maybe.just(data)
+            .map {
+                validate(it, onSuccess = { success?.invoke() }, onError = { message -> error?.invoke(message)
+                })
+            }
 }
