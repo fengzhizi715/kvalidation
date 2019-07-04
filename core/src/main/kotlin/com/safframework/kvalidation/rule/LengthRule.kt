773bb785123ebd6571fe2beb8e43a71837ec06f8
fengzhizi715 <fengzhizi715@126.com>
2019-07-04 12:55:16 +0800

    add LengthRule

---
+++ core/src/main/kotlin/com/safframework/kvalidation/rule/LengthRule.kt
@@ -0,0 +1,13 @@
+package com.safframework.kvalidation.rule
+
+import com.safframework.kvalidation.ValidateRule
+
+/**
+ * Created by tony on 2019-07-04.
+ */
+class LengthRule(private val min: Int, private val max: Int) : ValidateRule<String> {
+
+    override fun errorMessage() = "expected text length between: $min - $max"
+
+    override fun validate(data: String) = data.length in min..max
+}
--- example/src/main/java/TestPassword.kt
+++ example/src/main/java/TestPassword.kt
@@ -1,7 +1,6 @@
 import com.safframework.kvalidation.defineValidator
 import com.safframework.kvalidation.rule.MinLengthRule
 import com.safframework.kvalidation.rule.PatternRule
-import java.util.regex.Pattern
 
 /**
  * Created by tony on 2019-07-04.
@@ -10,9 +9,9 @@ fun main() {
 
     val validator = defineValidator<String>{
 
-        this addRule MinLengthRule(6)
+        this addRule MinLengthRule(6)                                       // 密码长度不能小于6位
 
-        this addRule PatternRule("^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]+$")
+        this addRule PatternRule("^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]+$") // 密码必须数字和字母的组合
     }
 
     val password = "123456a"
