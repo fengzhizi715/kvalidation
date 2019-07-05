d174ce88fbd435bef1459f35d7019141272ae834
fengzhizi715 <fengzhizi715@126.com>
2019-07-05 23:00:27 +0800

    update test case

--- example/src/main/java/TestEmailRule.kt
+++ example/src/main/java/TestEmailRule.kt
@@ -1,5 +1,6 @@
 import com.safframework.kvalidation.defineValidator
 import com.safframework.kvalidation.rule.EmailRule
+import com.safframework.kvalidation.validate
 
 /**
  * Created by tony on 2019-07-04.
@@ -13,7 +14,16 @@ fun main() {
 
     val email = "fengzhizi715@126.com"
 
-    val result = validator.validate(email,onError = { println(it)})
+    val result1 = validator.validate(email,onError = { println(it)})
 
-    println(result)
+    println(result1)
+
+    val invalidEmail = "fengzhizi715@126"
+
+    val result2 = invalidEmail.validate{
+
+        this addRule EmailRule()
+    }
+
+    println(result2)
 }
